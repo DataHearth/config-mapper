29a9a0310397ffba34b1d8b2c71a7c38bf54e423
DataHearth <antoine.l@antoine-langlois.net>
2022-02-25 00:17:02 +0100

    feat(cli): implement pkgs installation

--- internal/configuration.go
+++ internal/configuration.go
@@ -1,5 +1,13 @@
 package mapper
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/pterm/pterm"
+	"github.com/spf13/viper"
+)
+
 type Configuration struct {
 	Storage         Storage     `yaml:"storage"`
 	Files           []string    `yaml:"files"`
@@ -22,4 +30,34 @@ type PkgManagers struct {
 	InstallationOrder []string    `yaml:"installation-order"`
 	Homebrew          []string    `yaml:"homebrew"`
 	AptGet            interface{} `yaml:"apt-get"`
+}
+
+func InitConfig() {
+	h, err := os.UserHomeDir()
+	if err != nil {
+		errLogger.Printf("can't get home directory through $HOME variable: %v\n", err)
+		os.Exit(1)
+	}
+
+	if c := os.Getenv("CONFIG_MAPPER_CFG"); c != "" {
+		viper.AddConfigPath(c)
+	} else {
+		viper.AddConfigPath(h)
+	}
+
+	viper.SetConfigType("yml")
+	viper.SetConfigName(".config-mapper")
+
+	viper.SetDefault("storage.location", fmt.Sprintf("%s/config-mapper", os.TempDir()))
+	viper.SetDefault("package-managers.installation-order", []string{"apt", "homebrew"})
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			errLogger.Println(pterm.Red(err))
+		} else {
+			errLogger.Printf(pterm.Red(fmt.Sprintf("failed to read config: %v\n", err)))
+		}
+
+		os.Exit(1)
+	}
 }
--- internal/git.go
+++ internal/git.go
@@ -1,4 +1,4 @@
-package git
+package mapper
 
 import (
 	"errors"
