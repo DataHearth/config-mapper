5141ccdd3fc42bd57fb24435f6238eb6ce4b6cda
DataHearth <dev@antoine-langlois.net>
2024-07-31 11:42:30 +0200

    refactor(logging): drop pterm

--- internal/pkgs.go
+++ internal/pkgs.go
@@ -8,7 +8,7 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/pterm/pterm"
+	"github.com/fatih/color"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v3"
 )
@@ -22,18 +22,18 @@ var (
 )
 
 func LoadPkgs(c PkgManagers) error {
-	pterm.DefaultSection.Println("Load folders into saved location")
+	color.Blue("# Load folders into saved location")
 
 	for _, pkg := range c.InstallationOrder {
 		switch pkg {
 		case "homebrew":
 			if err := installBrewPkgs(c.Homebrew); err != nil {
-				errLogger.Println(pterm.Red(err))
+				PrintError(err.Error())
 				return ErrFailedInstallation
 			}
 		case "apt":
 			if err := installAptPkgs(c.Aptitude); err != nil {
-				errLogger.Println(pterm.Red(err))
+				PrintError(err.Error())
 				return ErrFailedInstallation
 			}
 		}
@@ -43,13 +43,13 @@ func LoadPkgs(c PkgManagers) error {
 }
 
 func SavePkgs(cfg Configuration) error {
-	pterm.DefaultSection.Println("Save user installed packages")
+	color.Blue("# Save user installed packages")
 
 	for _, pkg := range cfg.PackageManagers.InstallationOrder {
 		switch pkg {
 		case "homebrew":
 			if err := SaveBrewPkgs(cfg); err != nil {
-				errLogger.Println(pterm.Red(err))
+				PrintError(err.Error())
 				return ErrFailedSaving
 			}
 		case "apt":
@@ -65,7 +65,7 @@ func SaveBrewPkgs(cfg Configuration) error {
 		return err
 	}
 
-	introSpinner, _ := pterm.DefaultSpinner.WithShowTimer(true).WithRemoveWhenDone(false).Start("Installing homebrew packages")
+	color.Blue("## Saving Homebrew packages")
 
 	o, err := exec.Command("brew", "leaves", "--installed-on-request").Output()
 	if err != nil {
@@ -84,9 +84,7 @@ func SaveBrewPkgs(cfg Configuration) error {
 		return err
 	}
 
-	introSpinner.Stop()
-	introSpinner.Success("Packages intalled succesfully")
-
+	color.Green("Packages saved succesfully !")
 	return nil
 }
 
@@ -96,22 +94,20 @@ func installBrewPkgs(pkgs []string) error {
 	}
 
 	if len(pkgs) == 0 {
-		pterm.Println(pterm.Blue("homebrew: nothing to do"))
+		fmt.Println("homebrew: nothing to do")
 		return nil
 	}
 
 	cmd := exec.Command("brew", "install")
 	cmd.Args = append(cmd.Args, pkgs...)
-	introSpinner, _ := pterm.DefaultSpinner.WithShowTimer(true).WithRemoveWhenDone(false).Start("Installing homebrew packages")
+	color.Blue("## Installing Homebrew packages")
 
 	if err := cmd.Run(); err != nil {
-		introSpinner.Stop()
-		introSpinner.SuccessPrinter.PrintOnErrorf("brew command failed", err)
+		PrintError("brew command failed: %v", err)
 		return err
 	}
 
-	introSpinner.Stop()
-	introSpinner.Success("Packages intalled succesfully")
+	color.Green("Packages intalled succesfully !")
 
 	return nil
 }
@@ -122,34 +118,21 @@ func installAptPkgs(pkgs []string) error {
 	}
 
 	if len(pkgs) == 0 {
-		pterm.Println(pterm.Blue("aptitude: nothing to do"))
+		fmt.Println("aptitude: nothing to do")
 		return nil
 	}
 
 	cmd := exec.Command("sudo", "apt-get", "install")
 	cmd.Args = append(cmd.Args, pkgs...)
 
-	introSpinner, _ := pterm.DefaultSpinner.WithShowTimer(true).WithRemoveWhenDone(false).Start("Installing aptitude packages")
+	color.Blue("## Installing aptitude packages")
 
-	chErr := make(chan error)
-	defer close(chErr)
-
-	go func(chErr chan error) {
-		if err := cmd.Run(); err != nil {
-			chErr <- err
-			return
-		}
-
-		chErr <- nil
-	}(chErr)
-
-	err := <-chErr
-	introSpinner.Stop()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
+		PrintError("aptitude command failed: %v", err)
 		return err
 	}
 
-	introSpinner.Success("Packages intalled succesfully")
+	color.Green("Packages intalled succesfully !")
 
 	return nil
 }
